1d5f073359f12f905e1d4c66df993bc0a753f82e
Srirangan <srirangan@gmail.com>
2012-03-08 15:40:21 +0530

    Complete service implementation

--- billing-system/lib/service.js
+++ billing-system/lib/service.js
@@ -2,54 +2,58 @@ var model = require("./model.js");
 
 module.exports = {
   add: function (bill, callback) {
-    model.bill.add(bill, callback);
+    model.bills.add(bill, callback);
   },
   modify: function (bill, callback) {
-    model.bill.modify(bill, callback);
+    model.bills.modify(bill, callback);
   },
   remove: function (billId, callback) {
-    model.bill.remove(billId, callback);
+    model.bills.remove(billId, callback);
   },
   removeAll: function (callback) {
-    model.bill.removeAll(callback);
+    model.bills.removeAll(callback);
   },
   list: function (callback) {
-    model.bill.list(callback);
+    model.bills.list(callback);
   },
   get: function (billId, callback) {
-    model.bill.get(billId, callback);
+    model.bills.get(billId, callback);
   },
 
   tags: {
     add: function (billId, tag, callback) {
-
+      model.tags.add(billId, tag, callback);
     },
     remove: function (billId, tag, callback) {
-
+      model.tags.remove(billId, tag, callback);
     }
   },
 
   documents: {
-    add: function (billId, documentId, callback) {
-
+    add: function (billId, document, callback) {
+      model.documents.add(billId, document, callback);
     },
-    remove: function (billId, documentId, callback) {
-
+    remove: function (billId, document, callback) {
+      model.documents.remove(billId, document, callback);
     }
   },
 
   comments: {
     add: function (billId, comment, callback) {
+      comment.billId = billId;
+      model.comments.add(comment, callback);
     },
-    edit: function (comment, callback) {
+    modify: function (comment, callback) {
+      model.comments.modify(comment, callback);
     },
     remove: function (commentId, callback) {
-    },
-    removeAll: function (billId, callback) {
+      model.comments.remove(commentId, callback);
     },
     list: function (billId, callback) {
+      model.comments.list(billId, callback);
     },
     get: function (commentId, callback) {
+      model.comments.get(commentId, callback);
     }
   }
-}
+};
