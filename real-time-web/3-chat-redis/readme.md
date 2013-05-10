befb72dd6a796a23e80c6a8839323bb144782912
Srirangan <srirangan@gmail.com>
2013-05-10 14:29:11 +0200

    3-chat-redis

---
+++ real-time-web/3-chat-redis/readme.md
@@ -0,0 +1,8 @@
+## 3-chat-rooms
+
+* everything from `../2-chat-rooms/readme.md`
+* and better pub-sub with redis
+
+#### :-/
+
+* `index.html:28` wtf?
---
+++ real-time-web/3-chat-redis/server.js
@@ -0,0 +1,39 @@
+(function () {
+  "use strict";
+
+  var http = require("http");
+  var fs = require("fs");
+  var WebSocketServer = require("websocket").server;
+  var redis = require("redis");
+
+  var static_server = http.createServer(function (req, res) {
+    res.writeHead(200);
+    var file = req.url.indexOf("index.css") > -1 ? "index.css" : "index.html";
+    res.write(fs.readFileSync(file));
+    res.end();
+  });
+
+  var store = {redis: redis.createClient(), subscriber: redis.createClient()};
+
+  var ws_server = new WebSocketServer({httpServer: static_server});
+
+  ws_server.on("request", function (req) {
+    var conn = req.accept(), channel = "channel-" + req.resource, list = "messages-" + req.resource;
+    store.redis.lrange(list, 0, -1, function (err, messages) {
+      conn.sendUTF(JSON.stringify(messages));
+    });
+    store.subscriber.subscribe(channel);
+    store.subscriber.on("message", function (channel, message) {
+      conn.sendUTF(JSON.stringify(message));
+    });
+    conn.on("message", function (message) {
+      store.redis.rpush(list, message.utf8Data);
+      store.redis.publish(channel, message.utf8Data);
+    });
+  });
+
+  static_server.listen(8080);
+
+  console.log("3-chat-redis app started..");
+
+})();
