c796394dabe3fbe563c544a4085ba70ad5813620
Srirangan <srirangan@gmail.com>
2013-06-25 17:17:59 +0200

    fixup1

--- real-time-web/1-chat-echo/server.js
+++ real-time-web/1-chat-echo/server.js
@@ -20,12 +20,8 @@
 
   ws_server.on("request", function (req) {
     var conn = req.accept();
-    conn.on("message", function (message) {
-      conn.sendUTF(message.utf8Data);
-    });
-    setInterval(function () {
-      conn.sendUTF("ping even five seconds ... " + new Date().toISOString())
-    }, 5000);
+    conn.on("message", function (message) { conn.sendUTF(message.utf8Data); });
+    setInterval(function () { conn.sendUTF("ping even five seconds ... " + new Date().toISOString()) }, 5000);
   });
 
   static_server.listen(8080);
